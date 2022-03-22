eab77d40012f360837674288248ef6633abb6a47
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2022-03-22 17:57:01 -0400

    Update RingTone.java

--- RingTone.java
+++ RingTone.java
@@ -9,45 +9,47 @@ public class RingTone {
     private int first;          // index for the next dequeue
     private int last;           // index for the next enqueues
 
-    // CONSTRUCTOR
     // load RingTone buffer object with a SINE WAVE at the given frequency
     public RingTone(double hz) {
 
-        // buffer index tracking
+        // init buffer index tracking
         first = 0;
         last = 0;
 
-        // YOU DO: sample a full period of the SINE WAVE made by the input frequency
+        // sample a full period of the SINE WAVE made by the input frequency
         double numSamples = StdAudio.SAMPLE_RATE / hz; // eg, 100.2
         bufferSize = (int) Math.round(numSamples);     // eg, 100
 
-        rb = new double[bufferSize];
+	    // WE DO: load the ring buffer with a full period of samples from the SINE WAVE
+	    rb = new double[bufferSize];
 
         for (int i = 0; i < bufferSize; i++) {
-
+		
+	        // divide full period of the SINE WAVE evenly between <bufferSize> steps
             double s = i * numSamples/bufferSize;
-            // the SINE WAVE made by the input frequency
+            
+	        // the SINE WAVE made by the input frequency
             this.enqueue(Math.sin(2 * Math.PI * s * hz / StdAudio.SAMPLE_RATE));
-	}
+	    }
     }
 
-    // WE DO: play this RingTone for the specified duration
+    // YOU DO: play this RingTone for the specified duration
     public void playTone(double duration) {
 
-        // WE DO: total number of samples to play in the specified duration
-	int N = (int) (StdAudio.SAMPLE_RATE * duration);
+        // YOU DO: total number of samples to play in the specified duration
+	    int N = (int) (StdAudio.SAMPLE_RATE * duration);
 
-	for (int i = 1; i <= N; i++) {
+	    for (int i = 1; i <= N; i++) {
 
-            // WE DO: dequeue a sample from this RingTone
+            // YOU DO: dequeue a sample from this RingTone
             double quanta = this.dequeue();
 
-            // WE DO: enqueue the sample back into the buffer @ .98 amplitude
+            // YOU DO: enqueue the sample back into the buffer @ .98 amplitude
             this.enqueue(quanta * .98);
 
-            // WE DO: play the sample
-	    StdAudio.play(quanta);
-	}
+            // YOU DO: play the sample
+	        StdAudio.play(quanta);
+	    }
     }   
 
     // YOU DO: add item x to index <last> and update index
