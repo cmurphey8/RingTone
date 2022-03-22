77466a23ab418c55f4c02b7edb685823ef50f1d2
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2022-03-22 18:01:06 -0400

    Update RingTone.java

--- RingTone.java
+++ RingTone.java
@@ -21,15 +21,11 @@ public class RingTone {
         bufferSize = (int) Math.round(numSamples);     // eg, 100
 
 	    // WE DO: load the ring buffer with a full period of samples from the SINE WAVE
-	    rb = new double[bufferSize];
-
         for (int i = 0; i < bufferSize; i++) {
 		
-	        // divide full period of the SINE WAVE evenly between <bufferSize> steps
-            double s = i * numSamples/bufferSize;
+	        // WE DO: divide full period of the SINE WAVE evenly between <bufferSize> steps
             
-	        // the SINE WAVE made by the input frequency
-            this.enqueue(Math.sin(2 * Math.PI * s * hz / StdAudio.SAMPLE_RATE));
+	        // WE DO: enqueue the SINE WAVE sample
 	    }
     }
 
@@ -37,31 +33,26 @@ public class RingTone {
     public void playTone(double duration) {
 
         // YOU DO: total number of samples to play in the specified duration
-	    int N = (int) (StdAudio.SAMPLE_RATE * duration);
+	    int N;
 
 	    for (int i = 1; i <= N; i++) {
 
             // YOU DO: dequeue a sample from this RingTone
-            double quanta = this.dequeue();
+            double sample;
 
             // YOU DO: enqueue the sample back into the buffer @ .98 amplitude
-            this.enqueue(quanta * .98);
 
             // YOU DO: play the sample
-	        StdAudio.play(quanta);
 	    }
     }   
 
     // YOU DO: add item x to index <last> and update index
     private void enqueue(double x) {
-        rb[last] = x;
-        last = (last + 1) % rb.length;
     }
 
     // YOU DO: return item from index <first> and update index
     private double dequeue() {
-        double ret = rb[first];
-        first = (first + 1) % rb.length;
+        double ret;
         return ret;
     }
 }
